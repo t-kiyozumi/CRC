26ba1f8e6680554b88599b1336859e54cbb961a9
t-kiyozumi <k.takaki@gmail.com>
2020-08-22 16:34:12 +0900

    generalize the program

--- main.cpp
+++ main.cpp
@@ -1,43 +1,45 @@
 #include <stdio.h>
 #include <stdint.h>
-class message
-{
-public:
-};
-
+#define MESSAGE 0b00000000000000000011010011101100 //送りたい情報のマクロ
+#define GP 0b00000000000000000000000000001011      //生成多項式のマクロ
 
 int main()
 {
-    uint32_t Ms; //送りたい情報
-    uint32_t c;  //パリティ
-    uint32_t g;  //生成多項式
-    uint32_t mask;
+    uint32_t Ms;  //送りたい情報
+    uint32_t csc; //パリティ
+    uint32_t g;   //生成多項式
+    int Ng;       //生成多項式の最高次数
 
-    Ms = 0b00000000000000000011010011101100;
-    g = 0b00000000000000000000000000001011;
-    mask = 0b10000000000000000000000000000000;
+    uint32_t CDmask; //繰り下げを行うためのマスク
+    CDmask = 0b10000000000000000000000000000000;
 
-    Ms = Ms << 3;
-    g = g << (32 - 4);
+    //送りたい情報を決定
+    Ms = MESSAGE;
+    //生成多項式を決定
+    g = GP;
+    Ng = 4;
 
-    c = Ms;
+    Ms = Ms << (Ng - 1); //余りを後からくっつける分だけ下位ビットを開ける
+    g = g << (32 - Ng);
+
+    csc = Ms;
     while (1)
     {
-        if (c & mask) //先頭のビットが1であるならばxorの処理を行う
+        if (csc & CDmask) //先頭のビットが1であるならばxorの処理を行う
         {
-            c = c ^ g;
+            csc = csc ^ g;
         }
         else //先頭のビットが0ならば，gを右側にビットシフトする
         {
             g = g >> 1;
-            mask = mask >> 1;
+            CDmask = CDmask >> 1;
         }
 
-        if (c < 0b00000000000000000000000000001011)
+        if (csc < GP) //余りが生成多項式より小さくなったらプログラムを終了
         {
             break;
         }
     }
-    printf("%d", c);
+    printf("%d", csc);
     return 0;
 }
